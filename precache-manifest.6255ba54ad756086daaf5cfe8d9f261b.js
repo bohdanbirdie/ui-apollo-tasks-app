e90786bcf7d87593b6ac8f989d787ba41389f361
bohdanbirdie <voja4ok@yahoo.com>
2020-04-01 23:56:01 +0300

    Updates

---
+++ precache-manifest.6255ba54ad756086daaf5cfe8d9f261b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37053ce7008683b149cfb10df03bb8e2",
+    "url": "/ui-apollo-tasks-app/index.html"
+  },
+  {
+    "revision": "d744e7883b45457d689a",
+    "url": "/ui-apollo-tasks-app/static/css/2.ad8fe80d.chunk.css"
+  },
+  {
+    "revision": "bc1cd2fc3ffc166f634d",
+    "url": "/ui-apollo-tasks-app/static/css/main.92324744.chunk.css"
+  },
+  {
+    "revision": "d744e7883b45457d689a",
+    "url": "/ui-apollo-tasks-app/static/js/2.d5eb7c25.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/ui-apollo-tasks-app/static/js/2.d5eb7c25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1cd2fc3ffc166f634d",
+    "url": "/ui-apollo-tasks-app/static/js/main.460cf93e.chunk.js"
+  },
+  {
+    "revision": "4dc60596af721e20de91",
+    "url": "/ui-apollo-tasks-app/static/js/runtime-main.2ec22418.js"
+  }
+]);
